2990f4517df4a7a7c400300621a265c3ddbf0748
Inusha <inusha@outlook.com>
2020-10-31 17:34:38 +0000

    Create proxy.userscript.js

---
+++ proxy.userscript.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         JoinHook
+// @include      *
+// @run-at       document-start
+// ==/UserScript==
+// for this to run on local html files, allow tamper monkey to access local files
+(function() {
+	'use strict';
+	Array.prototype.join = new Proxy(Array.prototype.join, {
+		apply: function(target, _this, _arguments) {
+			return target.apply(_this, _arguments);
+	}});
+	console.warn('loaded JoinHook');
+})();

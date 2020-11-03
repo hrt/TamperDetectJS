d88b48dd9340d5582f25b041f5250004ebb1195a
Inusha <inusha@outlook.com>
2020-11-03 12:28:41 +0000

    Update concealed_proxy.userscript.js

--- concealed_proxy.userscript.js
+++ concealed_proxy.userscript.js
@@ -26,27 +26,39 @@
     redirections.set(Array.prototype.join, Array_join);
 
     function conceal(parent, key) {
-        const og = parent[key];
-        parent[key] = new Proxy(og, {
-            apply: function(target, _this, _arguments) {
-                var original_fn = redirections.get(_this);
-                if (original_fn) {
-                    _this = original_fn;
-                }
-                return og.apply(_this, _arguments);
+        try {
+            const og = parent[key];
+            if (redirections.get(parent[key])) {
+                // parent[key] is already a proxy
+                return;
             }
-        })
-        redirections.set(parent[key], og);
+            parent[key] = new Proxy(og, {
+                apply: function(target, _this, _arguments) {
+                    var original_fn = redirections.get(_this);
+                    if (original_fn) {
+                        _this = original_fn;
+                    }
+                    return og.apply(_this, _arguments);
+                }
+            })
+            redirections.set(parent[key], og);
+        } catch(e) {
+
+        }
     };
     var descriptors = Object.getOwnPropertyDescriptors(window);
     for (var key in descriptors) {
         try {
+            conceal(window[key].prototype, 'sort'); // not viable to find each case
+            conceal(window[key].prototype, 'slice');
             conceal(window[key].prototype, 'toString');
             conceal(window[key].prototype, 'toLocaleString');
             conceal(window[key].prototype, 'toDateString');
             conceal(window[key].prototype, 'toLocaleDateString');
             conceal(window[key].prototype, 'toLocaleTimeString');
-        } catch(e) {}
+        } catch(e) {
+
+        }
     }
 
 })();
